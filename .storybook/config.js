e4bcd569e9f9dbf2fb4a65dd2d24a23f079915a3
Sofia Sousa <csofiamsousa@gmail.com>
2019-05-20 10:23:02 +0100

    chore: Change storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
+// import React from 'react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { create, themes } from '@storybook/theming';
+
+// Option defaults.
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+      brandTitle: 'LovData Pro',
+    })
+  },
+});
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
@@ -7,6 +18,6 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>);
+// addDecorator(storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>);
 
 configure(loadStories, module);
