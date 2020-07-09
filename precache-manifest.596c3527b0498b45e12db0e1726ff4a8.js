caf4122ad6842e7b99d18cdd9dd594853ef0c7a2
Sofia Sousa <csofiamsousa@gmail.com>
2020-07-09 09:21:30 +0100

    Updates

---
+++ precache-manifest.596c3527b0498b45e12db0e1726ff4a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a8b202ffefe467a7eb371249169cca",
+    "url": "/lovdata-landingpage/index.html"
+  },
+  {
+    "revision": "8c47a8701f58d1235639",
+    "url": "/lovdata-landingpage/static/css/main.bf3138f2.chunk.css"
+  },
+  {
+    "revision": "5cba8a8ceb1ed2581d23",
+    "url": "/lovdata-landingpage/static/js/2.04d30675.chunk.js"
+  },
+  {
+    "revision": "8c47a8701f58d1235639",
+    "url": "/lovdata-landingpage/static/js/main.7bc63a35.chunk.js"
+  },
+  {
+    "revision": "1f44a647884fcd77cfbe",
+    "url": "/lovdata-landingpage/static/js/runtime~main.d4871d19.js"
+  },
+  {
+    "revision": "63f9e85acd5f75ff050441bf589fd2d5",
+    "url": "/lovdata-landingpage/static/media/SF-UI-Display-Black.63f9e85a.otf"
+  },
+  {
+    "revision": "61b0830fbbab84edf0ab1015acc596a5",
+    "url": "/lovdata-landingpage/static/media/SF-UI-Display-Heavy.61b0830f.otf"
+  },
+  {
+    "revision": "2ceadd10ca85adca14362906f2379fdc",
+    "url": "/lovdata-landingpage/static/media/SF-UI-Display-Regular.2ceadd10.otf"
+  },
+  {
+    "revision": "fc7876c5b37b4acf6d4a8029cf3166c9",
+    "url": "/lovdata-landingpage/static/media/SF-UI-Display-Semibold.fc7876c5.otf"
+  },
+  {
+    "revision": "7924cd988c85948467860c0b926d19f6",
+    "url": "/lovdata-landingpage/static/media/background.7924cd98.svg"
+  },
+  {
+    "revision": "9aa7fa0a422da878dece4b9a8a2ef22d",
+    "url": "/lovdata-landingpage/static/media/background.9aa7fa0a.svg"
+  },
+  {
+    "revision": "faf0a378311a96be21a02324f3ae26dc",
+    "url": "/lovdata-landingpage/static/media/background.faf0a378.svg"
+  },
+  {
+    "revision": "5251cf135b5bd967d5f90c4888997297",
+    "url": "/lovdata-landingpage/static/media/icomoon.5251cf13.svg"
+  },
+  {
+    "revision": "9a804c6484e777bee2518280f3b852c9",
+    "url": "/lovdata-landingpage/static/media/icomoon.9a804c64.woff2"
+  },
+  {
+    "revision": "c707a8aa48e0f0cb62228b233a73f0a3",
+    "url": "/lovdata-landingpage/static/media/icomoon.c707a8aa.ttf"
+  },
+  {
+    "revision": "c850dcaffd065a17eb6c37d899f4b062",
+    "url": "/lovdata-landingpage/static/media/icomoon.c850dcaf.eot"
+  },
+  {
+    "revision": "cd830e1cb4efc568370535a2cc305537",
+    "url": "/lovdata-landingpage/static/media/icomoon.cd830e1c.woff"
+  },
+  {
+    "revision": "476ae0093bc3cb244cc00bd831bb188a",
+    "url": "/lovdata-landingpage/static/media/icon-check.476ae009.svg"
+  },
+  {
+    "revision": "0e7a1e494d9b790e672032fc4f76df32",
+    "url": "/lovdata-landingpage/static/media/logo-pro.0e7a1e49.png"
+  }
+]);
